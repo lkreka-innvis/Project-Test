af0a7b54b8187777091de9ea8594dabddd2ca87f
Laxarela <lela.kreka@gmail.com>
2022-10-24 20:45:11 +0200

    changes

--- Accordians/javascript/script.js
+++ Accordians/javascript/script.js
@@ -1,60 +1,40 @@
 const checkbox = document.querySelector("input[name=select]");
-const content = document.querySelectorAll(".accoridans-contnent");
-const title = document.querySelectorAll(".accoridans-title");
-const icon = document.querySelectorAll(".accoridans-icon")
+const content = document.getElementsByClassName("accoridans-contnent");
+const title = document.getElementsByClassName("accoridans-title");
+const icon = document.getElementsByClassName("accoridans-icon")
 const info = document.querySelector(".accoridans-info")
 
-// checkbox.addEventListener('click',()=>{
-//     for (let i = 0 ; i <content.length; i++) {
-//         for(let j = 0 ; j<content.length;j++){
-//             content[j].style.display = "block";
-//         }
-//         content[i].style.display = "block";
-//     }
-// })
+    for (let i = 0 ; i <content.length; i++) {
+        checkbox.addEventListener('click',()=>{
+          if(checkbox.checked ===true){
+            content[i].style.display = "block"
+          }
+          else{
+            content[i].style.display = "none"
+          }
+        })
 
-// if(checkbox){
-//     for (let i = 0 ; i <content.length; i++) {
-//         title[i].addEventListener('click',()=>{
-//             for(let j = 0; j<content.length; j++){ 
-//                 content[j].style.display = "block";
-//                 content[i].style.display = "block";
-               
-//             }}
-//     )}
-// }
-
-
-
-
-for (let i = 0 ; i <content.length; i++) {
-
-    checkbox.addEventListener('click',()=>{
-        if(checkbox){
-            for(let j = 0; j<title.length; j++) {
-                title[i].addEventListener('click',()=>{
-                    content[i].style.display = "block";
-                })
-                content[j].style.display = "block";
+        title[i].addEventListener('click',()=>{
+            for(let j = 0; j<title.length; j++){
+             if(i===j){
+                content[i].style.display = content[i].style.display == "block" ? "none" : "block";
+    
+    
+                icon[i].classList.add("active");
+             }
+             else{
+                content[j].style.display = "none"
+                icon[j].classList.remove("active");
+    
+             }
             }
         }
-
-})
-    title[i].addEventListener('click',()=>{
-        for(let j = 0; j<title.length; j++){
-         if(i===j){
-            content[i].style.display = content[i].style.display == "block" ? "none" : "block";
+       
+    )};
 
 
-            icon[i].classList.add("active");
-         }
-         else{
-            content[j].style.display = "none"
-            icon[j].classList.remove("active");
-
-         }
-        }
-    }
-)};
 
 
+
+
+    
---
+++ Accordians/javascript/script2.js
@@ -0,0 +1,14 @@
+// const checkb = document.getElementById("box");
+// checkb.addEventListener("click", function(){
+//     for (i = 0; i < title.length; i++) {
+//         title[i].addEventListener("click", function() {
+//         //   this.classList.toggle("active");
+//           const content1 = this.nextElementSibling;
+//           if (content1.style.display == "block") {
+//             content1.style.display = "none";
+//           } else {
+//             content1.style.display = "block";
+//           }
+//         });
+//       }
+// });
