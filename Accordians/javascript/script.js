fbdb7ca1b9268e4b7271e0301c0a4c946d547f12
Laxarela <lela.kreka@gmail.com>
2022-10-25 18:18:08 +0200

    changes

--- Accordians/javascript/script.js
+++ Accordians/javascript/script.js
@@ -4,37 +4,45 @@ const title = document.getElementsByClassName("accoridans-title");
 const icon = document.getElementsByClassName("accoridans-icon")
 const info = document.querySelector(".accoridans-info")
 
-    for (let i = 0 ; i <content.length; i++) {
-        checkbox.addEventListener('click',()=>{
-          if(checkbox.checked ===true){
-            content[i].style.display = "block"
-          }
-          else{
-            content[i].style.display = "none"
-          }
-        })
+for (let i = 0; i < content.length; i++) {
+    checkbox.addEventListener('click',()=>{
+      if(checkbox.checked ===true){
+        content[i].style.display = "block"
+      }
+      else{
+        content[i].style.display = "none"
+      }
+    })
+    // checkbox.onclick = function () {
+    //     for (i = 0; i < title.length; i++) {
+    //                 //title[i].addEventListener("click", function() {
+    //              title.onclick = function(){
+    //                   const content1 = this.nextElementSibling;
+    //                   if (content1.style.display == "block") {
+    //                     content1.style.display = "none";
+    //                   } else {
+    //                     content1.style.display = "block";
+    //                   }
+    //                 };
+                
+    //               }
+    // }
 
-        title[i].addEventListener('click',()=>{
-            for(let j = 0; j<title.length; j++){
-             if(i===j){
-                content[i].style.display = content[i].style.display == "block" ? "none" : "block";
-    
-    
-                icon[i].classList.add("active");
-             }
-             else{
-                content[j].style.display = "none"
-                icon[j].classList.remove("active");
-    
-             }
+    title[i].addEventListener('click', () => {
+            for (let j = 0; j < title.length; j++) {
+                if (i === j) {
+                    content[i].style.display = content[i].style.display == "block" ? "none" : "block";
+
+
+                    icon[i].classList.add("active");
+                } else {
+                    content[j].style.display = "none"
+                    icon[j].classList.remove("active");
+
+                }
             }
         }
-       
-    )};
 
+    )
+};
 
-
-
-
-
-    
